cb379e3592ddfd023e2ea2d872508ec2648153fe
gurug-prog <ivanmysterys@gmail.com>
2022-07-08 23:33:05 +0300

    feat: implement horizontal snake traversal

--- MatrixTraversal/MatrixHandler.cs
+++ MatrixTraversal/MatrixHandler.cs
@@ -28,6 +28,27 @@ internal class MatrixHandler
         return matrix;
     }
 
+    public void TraverseHorizontalSnake(in string[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            if (i % 2 == 0)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i, j]} ");
+                }
+            }
+            else
+            {
+                for (int j = matrix.GetLength(1) - 1; j >= 0; j--)
+                {
+                    Console.Write($"{matrix[i, j]} ");
+                }
+            }
+        }
+    }
+
     public void PrintMatrix(in string[,] matrix)
     {
         for (int n = 0; n < matrix.GetLength(0); n++)
