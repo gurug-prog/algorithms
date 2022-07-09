48bb257de5e426894ec061ec036080103449bc38
gurug-prog <ivanmysterys@gmail.com>
2022-07-09 23:33:38 +0300

    feat: add horizontal snake usage

--- MatrixTraversal/Program.cs
+++ MatrixTraversal/Program.cs
@@ -2,4 +2,12 @@
 
 var mh = new MatrixHandler();
 var matrix = mh.GenerateMatrix(5);
+var lineDelimiter = new string('-', 40) + "\n";
+
+Console.WriteLine("Simple printing:");
+Console.WriteLine(lineDelimiter);
 mh.PrintMatrix(matrix);
+
+Console.WriteLine("\nHorizontal snake traversal:");
+Console.WriteLine(lineDelimiter);
+mh.TraverseHorizontalSnake(matrix);
