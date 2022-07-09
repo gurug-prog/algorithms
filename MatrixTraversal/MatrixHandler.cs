f86d25d0d4feb3b56ea3fbc07e4fcdf370923d78
gurug-prog <ivanmysterys@gmail.com>
2022-07-09 23:09:54 +0300

    feat: implement printing matrix

--- MatrixTraversal/MatrixHandler.cs
+++ MatrixTraversal/MatrixHandler.cs
@@ -2,5 +2,20 @@
 
 internal class MatrixHandler
 {
-    // TODO: matrix traversals
+    public MatrixHandler()
+    {
+
+    }
+
+	public void PrintMatrix(ref string[,] matrix)
+    {
+		for (int n = 0; n < matrix.GetLength(0); n++)
+		{
+			for (int m = 0; m < matrix.GetLength(1); m++)
+			{
+				Console.Write($"{matrix[n, m]} ");
+			}
+			Console.WriteLine();
+		}
+	}
 }
