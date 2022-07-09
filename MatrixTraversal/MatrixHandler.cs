32611ea0c88b8c2509fc62bf2f23688a68d4749f
gurug-prog <ivanmysterys@gmail.com>
2022-07-09 23:17:14 +0300

    feat: add test matrix generator

--- MatrixTraversal/MatrixHandler.cs
+++ MatrixTraversal/MatrixHandler.cs
@@ -4,18 +4,39 @@ internal class MatrixHandler
 {
     public MatrixHandler()
     {
-
     }
 
-	public void PrintMatrix(ref string[,] matrix)
+    public string[,] GenerateMatrix(int size)
     {
-		for (int n = 0; n < matrix.GetLength(0); n++)
-		{
-			for (int m = 0; m < matrix.GetLength(1); m++)
-			{
-				Console.Write($"{matrix[n, m]} ");
-			}
-			Console.WriteLine();
-		}
-	}
+        var matrix = new string[size, size];
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                var numElement = i * size + j;
+                if (numElement < 10)
+                {
+                    matrix[i, j] = $"0{numElement.ToString()}";
+                }
+                else
+                {
+                    matrix[i, j] = $"{numElement.ToString()}";
+                }
+            }
+        }
+
+        return matrix;
+    }
+
+    public void PrintMatrix(ref string[,] matrix)
+    {
+        for (int n = 0; n < matrix.GetLength(0); n++)
+        {
+            for (int m = 0; m < matrix.GetLength(1); m++)
+            {
+                Console.Write($"{matrix[n, m]} ");
+            }
+            Console.WriteLine();
+        }
+    }
 }
